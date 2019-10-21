7e60d6ae0f09a3c2320b8c0e31c99b147a0f0497
Gustavo Garcia <goostavobg@gmail.com>
2019-10-21 14:41:11 -0300

    Flutter initial project

---
+++ android/app/src/main/kotlin/com/example/table_fixed_header/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.table_fixed_header
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
